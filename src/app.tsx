c32bf68ec1deaa4704471cfa44bc43d43dddcb63
Marcos Anjos <dev.marcosanjos@gmail.com>
2024-02-10 21:24:26 -0300

    Salvando uma nota

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ interface Note {
 }
 
 export function App() {
-
+  const [search, setSearch] = useState('') // Estado para armazenar o valor do input de busca
   const [notes, setNotes] = useState<Note[]>(() => {
 
       const notesOnLocalStorage = localStorage.getItem('notes') // Busca as notas no localStorage
@@ -29,7 +29,7 @@ export function App() {
       date: new Date(),
       content
     }
-
+  
     const notesArray = [newNote, ...notes] // Cria um novo array com a nova nota no início
 
     setNotes(notesArray) // Adiciona a nova nota no início do array
@@ -37,6 +37,18 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray)) // Salva o array no localStorage
 
   }
+
+  // função para lidar com a busca de notas
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const qyery = event.target.value // Pega o valor do input de busca
+
+    setSearch(qyery) // Atualiza o estado de busca
+  }
+
+  // Filtra as notas com base na busca
+  const filteredNotes = search !== '' 
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) 
+    : notes
 
   return (
     <div className='max-w-6xl mx-auto my-12 space-y-6'>
@@ -46,6 +58,7 @@ export function App() {
           type="text" 
           placeholder='Busque em suas notas...' 
           className='w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500 '
+          onChange={handleSearch}
         />
       </form>
 
@@ -54,7 +67,7 @@ export function App() {
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        {notes.map(note => {
+        {filteredNotes.map(note => {
           return (
             <NoteCard note={note} key={note.id}  />
           )
